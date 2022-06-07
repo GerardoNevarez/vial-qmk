e57b876d731a8bf4da1c214de9cc196ff045a968
GerardoNevarez <GerardoNevarez@users.noreply.github.com>
2022-06-07 18:24:26 -0400

    Reviung41 with KB2040 support (untested)

--- keyboards/reviung/reviung41/config.h
+++ keyboards/reviung/reviung41/config.h
@@ -40,10 +40,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS \
-    { F4, F5, F6, F7, B1, B3, B2 }
-#define MATRIX_COL_PINS \
-    { D4, C6, D7, E6, B4, B5 }
+//#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3, B2 }
+//#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { 29, 28, 27, 26, 18, 20, 19 }
+#define MATRIX_COL_PINS { 4, 5, 6, 7, 8, 9 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
@@ -52,13 +52,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+//#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
 
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
 // #define BACKLIGHT_LEVELS 3
 
-#define RGB_DI_PIN D3
+//#define RGB_DI_PIN D3
+#define RGB_DI_PIN 0 
 #ifdef RGB_DI_PIN
   #define RGBLED_NUM 11
   #define RGBLIGHT_HUE_STEP 16
---
+++ keyboards/reviung/reviung41/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2B, 0x74, 0x7B, 0x51, 0x83, 0x62, 0x66, 0x01}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
