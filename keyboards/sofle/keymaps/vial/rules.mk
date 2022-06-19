GerardoNevarez/qmk_firmware

configure Vial for Sofle Choc

keyboards/sofle/keymaps/vial/rules.mk
@@ -6,8 +6,14 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_ENCODERS_ENABLE = yes
 LTO_ENABLE = yes
-RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+VIALRGB_ENABLE = yes
 QMK_SETTINGS = no
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
 COMBO_ENABLE = no
+TAP_DANCE_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
+BOOTMAGIC_ENABLE = no
+MAGIC_ENABLE = yes
+

GerardoNevarez/vial-qmk

keyboards/sofle/keymaps/vial/rules.mk
@@ -1,13 +1,30 @@
+LTO_ENABLE = yes
+
 OLED_ENABLE = yes
-ENCODER_ENABLE = yes
-CONSOLE_ENABLE = no
-EXTRAKEY_ENABLE = yes
+OLED_DRIVER = SSD1306
+
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
+
+ENCODER_ENABLE = yes
 VIAL_ENCODERS_ENABLE = yes
-LTO_ENABLE = yes
+
 RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+VIALRGB_ENABLE = yes
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+EXTRAKEY_ENABLE = yes
+SWAP_HANDS_ENABLE = no
+
 QMK_SETTINGS = no
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
 COMBO_ENABLE = no
+TAP_DANCE_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
+
+BOOTMAGIC_ENABLE = no
+MAGIC_ENABLE = no
+